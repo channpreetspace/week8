349425a994e67da9b12d7d777494abd6bd6a9ec4
Channpreet <164895875+channpreetspace@users.noreply.github.com>
2024-10-29 14:45:09 -0400

    sell

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.set('views', path.join(__dirname, 'views'));
 const config = {
     user: 'azureuser',
     password: 'P@ssw0rdssss',
-    server: 'week9176.database.windows.net',
-    database: 'week9176',
+    server: 'week8176.database.windows.net',
+    database: 'week8176',
     options: {
         encrypt: true,
         enableArithAbort: true
